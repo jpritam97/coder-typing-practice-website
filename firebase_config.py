48a8b042e6519bf435722dba1e95feecc310aa49
jpritam97 <jpritam97@gmail.com>
2025-08-04 00:13:48 +0530

    Add redacted firebase config

---
+++ firebase_config.py
@@ -0,0 +1,16 @@
+FIREBASE_CONFIG = {
+    "type": "service_account",
+    "project_id": "coder-typing-practice-website",
+    "private_key_id": "REDACTED",
+    "private_key": "REDACTED",
+    "client_email": "REDACTED",
+    "client_id": "REDACTED",
+}
+FIREBASE_WEB_CONFIG = {
+    "apiKey": "REPLACE_ME",
+    "authDomain": "coder-typing-practice-website.firebaseapp.com",
+    "projectId": "coder-typing-practice-website",
+    "storageBucket": "coder-typing-practice-website.appspot.com",
+    "messagingSenderId": "REPLACE_ME",
+    "appId": "REPLACE_ME"
+}
